73a93fd5d10d90689ac679232ddbda4d7b4b5131
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2015-02-11 13:27:22 +0100

    Add main project

---
+++ trymskvida_main/Assets/scripts/soundButtonScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Test {
+	public class soundButtonScript : MonoBehaviour {
+
+
+		public Sprite soundSprite;
+		public Sprite noSoundSprite;
+
+		private bool soundBool;
+		
+		private Button soundButton;
+
+		// Use this for initialization
+		void Start () {
+			soundButton = GetComponent<Button>();
+			soundBool = true;
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public void toggleSound(){
+			if (soundBool) {
+				soundButton.image.sprite = noSoundSprite;
+				soundBool = false;
+				AudioListener.pause = true;
+			} else {
+				soundButton.image.sprite = soundSprite;
+				soundBool = true;
+				AudioListener.pause = false;
+			}
+	}
+}
+}
