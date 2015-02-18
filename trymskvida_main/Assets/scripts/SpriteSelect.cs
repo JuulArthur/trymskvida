a7f8aac888846a47bd7b342e8a46a9fc020e500a
Sitron Te <halsaune@stud.ntnu.no>
2015-02-18 10:43:17 +0100

    Merged projects, new character select in main project

---
+++ trymskvida_main/Assets/scripts/SpriteSelect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CharacterCustomize {
+	public class SpriteSelect : MonoBehaviour {
+
+		public string Key;
+		public bool UpdateEachFrame = false;
+		private int val = -1;
+
+		public Sprite[] Sprites;
+
+		// Use this for initialization
+		void Start () {
+			UpdateSprite ();
+		}
+
+		void Update() {
+			if (UpdateEachFrame) {
+				UpdateSprite();
+			}
+		}
+
+		public void SelectSprite (int sprite) {
+			if (sprite < 0 || sprite >= Sprites.Length) {
+				PlayerPrefs.SetInt(Key, 0);
+			} else {
+				PlayerPrefs.SetInt(Key, sprite);
+			}
+			UpdateSprite();
+		}
+
+		private void UpdateSprite() {
+			if (PlayerPrefs.HasKey (Key)) {
+				int k = PlayerPrefs.GetInt(Key);
+				if (k != val) {
+					val = k;
+					if (k >= 0 && k < Sprites.Length) {
+						gameObject.GetComponent<SpriteRenderer>().sprite = Sprites[k];
+					}
+				}
+			}
+		}
+	}
+}
