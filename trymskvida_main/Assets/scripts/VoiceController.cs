bbbbb2dc492bdd501811791c7b70381e1c278a4f
Sitron Te <halsaune@stud.ntnu.no>
2015-03-04 12:43:36 +0100

    Voice is now present. Can be played by a push of a button

---
+++ trymskvida_main/Assets/scripts/VoiceController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoiceController : MonoBehaviour {
+
+	private AudioSource Source;
+
+	public AudioClip[] clips;
+	private int clipNumber = 0;
+
+	private bool play = false;
+
+	// Use this for initialization
+	void Start () {
+		Source = GameObject.Find ("VoiceSource").audio;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (play && !Source.isPlaying) {
+			clipNumber++;
+			play = clipNumber < clips.Length;
+			DoPlayThisClip ();
+		}
+	}
+
+	public void Play() {
+		play = true;
+		DoPlayThisClip ();
+	}
+
+	public void Stop() {
+		play = false;
+		Source.Stop ();
+	}
+
+	public void Rewind() {
+		clipNumber = 0;
+		Play ();
+	}
+
+	private void DoPlayThisClip () {
+		if (clipNumber < clips.Length) {
+			Source.clip = clips[clipNumber];
+			Source.Play ();
+		}
+	}
+}
