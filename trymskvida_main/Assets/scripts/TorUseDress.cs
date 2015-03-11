3b9c7b401bf8092cef49662ab2a9f94226a1c582
Sitron Te <halsaune@stud.ntnu.no>
2015-03-11 11:04:51 +0100

    Property usedress added on Tor

---
+++ trymskvida_main/Assets/scripts/TorUseDress.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CharacterCustomize {
+	public class TorUseDress : MonoBehaviour {
+
+		public bool UseDress {
+			get {
+				if (PlayerPrefs.HasKey (SpriteSelect.useDressKey)) {
+					PlayerPrefs.SetInt (SpriteSelect.useDressKey, 0);
+				}
+				return PlayerPrefs.GetInt(SpriteSelect.useDressKey) == 1;
+			}
+			set {
+				if (value)
+					PlayerPrefs.SetInt (SpriteSelect.useDressKey, 1);
+				else
+					PlayerPrefs.SetInt (SpriteSelect.useDressKey, 0);
+			}
+		}
+	}
+}
