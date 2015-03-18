4a152008c926bc3cf5f2bdd7b823c35b22f5718f
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2015-03-18 12:20:47 +0100

    lols

--- trymskvida_main/Assets/scripts/loadLevel.cs
+++ trymskvida_main/Assets/scripts/loadLevel.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class loadLevel : MonoBehaviour {
 
+	private AudioSource VoiceSource;
+
+	void Start () {
+		VoiceSource = GameObject.Find ("VoiceSource").audio;
+		}
+
 	public void loadGivenLevel(string level) {
 		Application.LoadLevel (level);
+		VoiceSource.Stop ();
 		}
 }
